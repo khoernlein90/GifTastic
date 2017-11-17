e8de065de1fb20f8f10f1312e17c05ee3640030f
Kyle Hoernlein <khoernlein90@gmail.com>
2017-11-16 23:16:47 -0500

    changed some js and css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,90 @@
-var animals = ["duck", "turtle", "gorilla", "giraffe", "mouse"];
+$(document).ready(function() {
+var movies = ["Tommy Boy", "The Departed", "Top Gun", "Road House", "Point Break", "Shawshank Redemption", "The Godfather", "Pulp Fiction", "Family Guy"];
 
-function displayAnimal(){
-var animal = $(this).attr("data-animal");
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+$(document).on("click", function(){
+  console.log(this)
+})
 
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response){
-        	console.log(response.data);
- 			var results = response.data;
+function displayMovie() {
+    $(".gifs-appear-here").empty();
+    var movie = $(this).attr("data-movie");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC&limit=10";
+$('.gifs-appear-here').append('<h2 class="result_title">Now Playing: <span>' + $(this).data('movie') + ' Gifs!</span></h2>' );
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+        console.log(response.data);
+        var results = response.data;
 
-          for (var i = 0; i < results.length; i++) {
+        for (var i = 0; i < results.length; i++) {
             var gifDiv = $("<div>");
+
+            // gifDiv.addClass('gif col-md-4 col-xs-12 col-sm-4');
+            // gifDiv.append('<img class="gifImg" src=' + results[i].images.fixed_height_still.url + ' data-still=' + results[i].images.fixed_height_still.url + ' data-animate=' + results[i].images.fixed_height.url + ' data-state="still"/>');
+            // gifDiv.append('<span class="label label-default">Rated: ' + results[i].rating + '</span>')
+
             var rating = results[i].rating;
             var p = $("<p>").text("Rating: " + rating);
 
-            var animalImage = $("<img>");
-            animalImage.addClass("gifClick");
-            animalImage.attr({
-                src: results[i].images.fixed_height.url,
-                "data-still": results[i].images.fixed_height_still.url,
-                "data-animate": results[i].images.fixed_height.url,
-                "data-state": "still",
-            });
+                var movieGif = $("<img>");
+                movieGif.addClass("gifClick");
+                movieGif.attr({
+                    src: results[i].images.fixed_height.url,
+                    "data-still": results[i].images.fixed_height_still.url,
+                    "data-animate": results[i].images.fixed_height.url,
+                    "data-state": "still",
+                });
 
             gifDiv.append(p);
-            gifDiv.append(animalImage);
-            $(".gifs-appear-here").prepend(gifDiv);
-          }
-          
-          $(".gifClick").on("click", function(){
+            gifDiv.append(movieGif);
+            $(".gifs-appear-here").append(gifDiv);
+        }
+        var row = $('div.gifs-appear-here > div');
 
-            	var state = $(this).attr("data-state");
-            	console.log(this);
-		        if (state == "still") {
-
-		            $(this).attr("data-state", "animate");
-		            $(this).attr("src", $(this).attr("data-animate"));
-		        }
-		        else {
-		            $(this).attr("data-state", "still");
-		            $(this).attr("src", $(this).attr("data-still"));          
-		        }
-            })
-        })
+        for (var j = 0; j < row.length; j += 2) {
+            row.slice(j, j + 2).wrapAll('<div class="row mx-auto"></div>');
+        }
+    })
 };
 
+$(document).on("click", ".gifClick", function() {
 
-function makeButtons(){
-	$(".buttons").empty();
-	for (var i = 0; i < animals.length; i++) {
-		var button = $("<button type='button' class='btn btn-success'>");
-		button.addClass("buttonClass");
-		button.attr("data-animal", animals[i]);
-		button.text(animals[i]);
-		$(".buttons").append(button);
-	}
+    var state = $(this).attr("data-state");
+    console.log(this);
+    if (state == "still") {
+
+        $(this).attr("data-state", "animate");
+        $(this).attr("src", $(this).attr("data-animate"));
+    } else {
+        $(this).attr("data-state", "still");
+        $(this).attr("src", $(this).attr("data-still"));
+    }
+})
+
+
+function makeButtons() {
+    $(".buttons").empty();
+    for (var i = 0; i < movies.length; i++) {
+        var button = $("<button type='button' class='btn'>");
+        button.addClass("buttonClass");
+        button.attr("data-movie", movies[i]);
+        button.append(movies[i]);
+        $(".buttons").append(button);
+    }
 }
 
-$(document).on("click", ".buttonClass", displayAnimal);
+$(document).on("click", ".buttonClass", displayMovie);
 
-$("#add-animal").on("click", function(event) {
-	event.preventDefault();
-	var newAnimal = $("#animal-input").val().trim();
-	if (newAnimal !== "") {
-		$("#animal-input").val("");
-		animals.push(newAnimal);
-	}
-	makeButtons();
+$("#add-movie").on("click", function(event) {
+    event.preventDefault();
+    var newMovie = $("#movie-input").val().trim();
+    if (newMovie !== "") {
+        $("#movie-input").val("");
+        movies.push(newMovie);
+    }
+    makeButtons();
 })
-	makeButtons();
+makeButtons();
+
+});
